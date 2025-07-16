85971e35ffd076ec4e55026f6586be345202b54c
권지훈 <rnjswlgns444@naver.com>
2025-07-16 12:10:42 +0900

    "main화면 테두리 추가"

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -9,18 +9,20 @@ export default ()=> {
     };
   return (
     <div className="bg">
-      <h1 className="tarot-title">TAROT</h1>
-      <div className="card-row">
-        {/* 왼쪽 카드 (왼쪽 3cm 이동) */}
-        <div className="tarot-card left-card">
-          <img src="/card_back.png" alt="카드 뒷면" className="card-image" />
+      <div className="tarot-frame">
+        <h1 className="tarot-title">TAROT</h1>
+        <div className="card-row">
+          {/* 왼쪽 카드 */}
+          <div className="tarot-card left-card">
+            <img src="/card_back.png" alt="카드 뒷면" className="card-image" />
+          </div>
+          {/* 오른쪽 카드 */}
+          <div className="tarot-card right-card">
+            <img src="/card_back2.png" alt="카드 뒷면" className="card-image" />
+          </div>
         </div>
-        {/* 오른쪽 카드 (오른쪽 3cm 이동) */}
-        <div className="tarot-card right-card">
-          <img src="/card_back2.png" alt="카드 뒷면" className="card-image" />
-        </div>
+        <button className="start-btn" onClick={handleClick}>START</button>
       </div>
-      <button className="start-btn" onClick={handleClick}>START</button>
     </div>
   );
-}
+};
