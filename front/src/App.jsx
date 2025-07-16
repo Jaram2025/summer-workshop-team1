4356f5216db49ecb52d92bb65b628b095148e2be
noturavrigk <ksm92038386@gmail.com>
2025-07-16 12:02:47 +0900

    Fix: card select page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,22 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css';
 import Chatting from './pages/Chatting';
 import Main from './pages/Main';
 import NotFoundPage from './pages/NotFoundPage';
+import CardSelector from './pages/CardSelector';
+
 
 function App() {
 
   return (
     <div className='container'>
+
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/chat" element={<Chatting />} />
-      <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/cardSelector" element={<CardSelector />} />
     </Routes>
     </div>
   )
