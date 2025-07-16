52d0ab25fa8c647617f5298416f3b47a0ea7e1a7
mokmokahr <jbmok66@gmail.com>
2025-07-16 14:14:07 +0900

    Feat: Add 404 page

--- front/src/pages/NotFoundPage.jsx
+++ front/src/pages/NotFoundPage.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-export default ()=>{
-    return(
-        <>
-        <div>404</div>
-        </>
-    )
+export default () => {
+    return (
+        <div style={{ width: "100%", height: "100%", display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <img src="card_04.png" alt="" srcset="" />
+            <h1>Not found</h1>
+            <h1>404</h1>
+        </div>
+    );
 }
