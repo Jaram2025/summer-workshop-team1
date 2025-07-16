5531d3b3737f0f3804ec480971beda6423586a09
권지훈 <rnjswlgns444@naver.com>
2025-07-16 15:28:44 +0900

    "결과 페이지 수정"

--- front/src/pages/Result.jsx
+++ front/src/pages/Result.jsx
@@ -1,6 +1,9 @@
 // Result.jsx
-import { useLocation, useNavigate } from "react-router-dom";
 import '../css/result.css';
+
+import { useState,useEffect,useRef } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Result() {
   // 더미 데이터(실제 데이터로 대체 가능)
@@ -9,6 +12,25 @@ export default function Result() {
     { name: "TEMPERANCE", image: "/card2.png" },
     { name: "DEATH", image: "/card3.png" }
   ];
+
+  function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    const navigate = useNavigate();
+    const [input, setInput] = useState("");
+    const [commentRecord,setCommentRecord] = useState([{type:"model",context:"안녕하세요! 타로를 봐드리겠습니다. 고민거리를 말해주세요!"}]);
+    const scrollRef = useRef(null);
+  const appendComment = async (e)=>{
+        const question = input;
+        e.preventDefault();
+        if (!input.trim()) return;
+        setCommentRecord((prev)=>[...prev, {type:"user",context:input}]);
+        setInput('');
+        setCommentRecord((prev)=>[...prev,{type:"client",context:"답변을 생각하고 있습니다..."}])
+        await sleep(1000)
+        setCommentRecord((prev)=>[...prev.slice(0,prev.length-1),{type:"client",context:"말씀 해주셔서 감사합니다! 고민을 생각하며 카드 3장을 뽑아주세요"}])
+        //await sendQuery(question,commentRecord);
+    }
 
   const summary = "지금은 큰 변화의 시기입니다. 두려워하지 말고 새로운 시작을 위해 균형잡힌 마음으로 나아가세요. 과거에 얽매이지 말고 미래를 향해 용기있게 발걸음을 내딛으시길 바랍니다.";
 
@@ -34,16 +56,10 @@ export default function Result() {
       </div>
       
       {/* 하단 입력창 */}
-      <div className="result-summary-title">
-        <div className="result-summary-text">
-          <input 
-            type="text" 
-            placeholder="질문을 입력하세요"
-            readOnly
-          />
-          <button>전송</button>
-        </div>
-      </div>
+        <form className="promptBox" onSubmit={appendComment}>
+            <input className="prompt-input" placeholder="질문을 입력하세요" type="text" value={input} onChange={(e)=>{setInput(e.target.value)}}/>
+            <button type="submit" className="prompt-send" >전송</button>
+        </form>
     </div>
   );
 }
