cf8a9ecb6cc3cb7bc89ed6713e8265dbd8d68621
mokmokahr <jbmok66@gmail.com>
2025-07-17 11:54:20 +0900

    Chore: fix code format

--- front/src/pages/NotFoundPage.jsx
+++ front/src/pages/NotFoundPage.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 export default () => {
     return (
         <div style={{ width: "100%", height: "100%", display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-            <img src="card_04.png" alt="" srcset="" />
+            <style>
+                {`
+                @keyframes spin {
+                    from { transform: rotate(0deg); }
+                    to { transform: rotate(360deg); }
+                }
+                .spin {
+                    animation: spin 2s linear infinite;
+                    background-color:purple;
+                }
+                `}
+            </style>
+            <img src="card_04.png" className='spin' alt="" srcset="" />
             <h1>Not found</h1>
             <h1>404</h1>
         </div>
